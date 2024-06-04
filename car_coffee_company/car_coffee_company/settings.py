7f82ba923c35adf1c5252c9752efa78ec247d564
YishakG <111497293+YishakG@users.noreply.github.com>
2024-06-04 10:52:49 +0300

    base file

--- car_coffee_company/car_coffee_company/settings.py
+++ car_coffee_company/car_coffee_company/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'inventory',
-]
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -126,3 +125,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# myproject/settings.py
--- car_coffee_company/inventory/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Car, Coffee, ImportTransaction, ExportTransaction
-
-admin.site.register(Car)
-admin.site.register(Coffee)
-admin.site.register(ImportTransaction)
-admin.site.register(ExportTransaction)
